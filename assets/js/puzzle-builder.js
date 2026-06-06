3927aeac71beb511564bc95ea675a1994c00a885
Pipo42 <adrian.toll.95@gmail.com>
2026-06-06 22:54:34 +0200

    Update puzzle-builder.js

--- assets/js/puzzle-builder.js
+++ assets/js/puzzle-builder.js
@@ -255,7 +255,7 @@ var PuzzleBuilder = (function () {
         playPuzzle(stageEl, exampleEl, exTextEl, cfg.pieces, cfg.example || '');
         obs.disconnect();
       }
-    }, { threshold: 0.6 });
+    }, { rootMargin: '-20% 0px -20% 0px', threshold: 0 });
 
     obs.observe(trigger);
   }
